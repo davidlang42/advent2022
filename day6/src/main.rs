ccccd1ef2e2360ef4802ef82fcaa34a6947befa9
David Lang <davidlang42@gmail.com>
2022-12-07 11:28:55 +1100

    day6 part1

---
+++ day6/src/main.rs
@@ -0,0 +1,32 @@
+use std::env;
+use std::fs;
+use std::collections::HashSet;
+use std::collections::VecDeque;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 2 {
+        let filename = &args[1];
+        let text = fs::read_to_string(&filename)
+            .expect(&format!("Error reading from {}", filename));
+        for line in text.split("\r\n") {
+            println!("Result: {}", find_unique_string(line, 4));
+        }
+    } else {
+        println!("Please provide 1 argument: Filename");
+    }
+}
+
+fn find_unique_string(line: &str, size: usize) -> isize {
+    let mut list: VecDeque<char> = VecDeque::new();
+    for (i, c) in line.chars().enumerate() {
+        list.push_back(c);
+        if list.len() > size {
+            list.pop_front();
+        }
+        if list.iter().collect::<HashSet<&char>>().len() == size {
+            return (i + 1).try_into().unwrap();
+        }
+    }
+    -1
+}
