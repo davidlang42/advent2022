94de5572e56e8f1c8520d1812e2682e3127ad35f
David Lang <davidlang42@gmail.com>
2022-12-13 17:23:29 +1100

    day11 part2 math hack

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -4,20 +4,20 @@ use std::fs;
 use std::str::FromStr;
 
 struct Monkey {
-    throw_count: u32,
-    items: VecDeque<f32>,
+    throw_count: u64,
+    items: VecDeque<u64>,
     operation: Operation,
     test: Test
 }
 
 enum Operation {
-    Multiply(f32),
-    Add(f32),
+    Multiply(u64),
+    Add(u64),
     Square
 }
 
 struct Test {
-    divisible_by: f32,
+    divisible_by: u64,
     true_index: usize,
     false_index: usize
 }
@@ -30,25 +30,26 @@ fn main() {
             .expect(&format!("Error reading from {}", filename));
         // part1
         let mut monkies: Vec<Monkey> = text.split("\r\n\r\n").map(|s| s.parse().unwrap()).collect();
-        // for _ in 0..20 {
-        //     run_round(&mut monkies, 3);
-        // }
-        // let mut throws: Vec<f32> = monkies.iter().map(|m| m.throw_count).collect();
-        // throws.sort();
-        // throws.reverse();
-        // println!("Monkey business part1: {}", throws.iter().take(2).product::<f32>());
-        // part2
-        // monkies = text.split("\r\n\r\n").map(|s| s.parse().unwrap()).collect();
+        let allowable_fudge_modulo: u64 = monkies.iter().map(|m| m.test.divisible_by).product();
         for _ in 0..20 {
-            run_round(&mut monkies, 1.0);
+            run_round(&mut monkies, 3, allowable_fudge_modulo);
+        }
+        let mut throws: Vec<u64> = monkies.iter().map(|m| m.throw_count).collect();
+        throws.sort();
+        throws.reverse();
+        println!("Monkey business part1: {}", throws.iter().take(2).product::<u64>());
+        // part2
+        monkies = text.split("\r\n\r\n").map(|s| s.parse().unwrap()).collect();
+        for _ in 0..10000 {
+            run_round(&mut monkies, 1, allowable_fudge_modulo);
         }
         for (i, m) in monkies.iter().enumerate() {
             println!("Monky {} has thrown {} items", i, m.throw_count);
         }
-        let mut throws: Vec<u32> = monkies.iter().map(|m| m.throw_count).collect();
+        let mut throws: Vec<u64> = monkies.iter().map(|m| m.throw_count).collect();
         throws.sort();
         throws.reverse();
-        println!("Monkey business part2: {}", throws.iter().take(2).product::<u32>());
+        println!("Monkey business part2: {}", throws.iter().take(2).product::<u64>());
     } else {
         println!("Please provide 1 argument: Filename");
     }
@@ -87,11 +88,11 @@ impl FromStr for Operation {
         Ok(if words[4].eq("old") {
             match words[3] {
                 "*" => Operation::Square,
-                "+" => Operation::Multiply(2.0),
+                "+" => Operation::Multiply(2),
                 _ => panic!("Invalid operation")
             }
         } else {
-            let literal: f32 = words[4].parse().unwrap();
+            let literal: u64 = words[4].parse().unwrap();
             match words[3] {
                 "*" => Operation::Multiply(literal),
                 "+" => Operation::Add(literal),
@@ -101,11 +102,11 @@ impl FromStr for Operation {
     }
 }
 
-fn run_round(monkies: &mut Vec<Monkey>, worry_decrease_factor: f32) {
+fn run_round(monkies: &mut Vec<Monkey>, worry_decrease_factor: u64, allowable_fudge_modulo: u64) {
     for i in 0..monkies.len() {
-        let mut self_throw: VecDeque<f32> = VecDeque::new();
+        let mut self_throw: VecDeque<u64> = VecDeque::new();
         while let Some(item) = monkies[i].items.pop_front() {
-            let new_item = run_operation(item, &monkies[i].operation, worry_decrease_factor);
+            let new_item = run_operation(item, &monkies[i].operation, worry_decrease_factor, allowable_fudge_modulo);
             let new_monkey = run_test(new_item, &monkies[i].test);
             monkies[new_monkey].items.push_back(new_item);
             monkies[i].throw_count += 1;
@@ -114,17 +115,17 @@ fn run_round(monkies: &mut Vec<Monkey>, worry_decrease_factor: f32) {
     }
 }
 
-fn run_operation(old: f32, operation: &Operation, worry_decrease_factor: f32) -> f32 {
+fn run_operation(old: u64, operation: &Operation, worry_decrease_factor: u64, allowable_fudge_modulo: u64) -> u64 {
     let new = match operation {
-        Operation::Square => old * old,
+        Operation::Square => (old % allowable_fudge_modulo).pow(2),
         Operation::Add(delta) => old + delta,
-        Operation::Multiply(factor) => old * factor
+        Operation::Multiply(factor) => (old % allowable_fudge_modulo) * factor
     };
     new / worry_decrease_factor
 }
 
-fn run_test(value: f32, test: &Test) -> usize {
-    if value % test.divisible_by == 0.0 {
+fn run_test(value: u64, test: &Test) -> usize {
+    if value % test.divisible_by == 0 {
         test.true_index
     } else {
         test.false_index
