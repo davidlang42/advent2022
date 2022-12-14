5c53356a0c12e710a1e9c7d1d5712618bdba8914
David Lang <davidlang42@gmail.com>
2022-12-14 17:25:54 +1100

    refactor

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -18,13 +18,15 @@ struct Path {
     points: Vec<Point>
 }
 
+const NL: &str = "\n";
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() == 2 {
         let filename = &args[1];
         let text = fs::read_to_string(&filename)
             .expect(&format!("Error reading from {}", filename));
-        let paths: Vec<Path> = text.split("\r\n").map(|s| s.parse().unwrap()).collect();
+        let paths: Vec<Path> = text.split(NL).map(|s| s.parse().unwrap()).collect();
         let mut grid: HashMap<Point,Fill> = HashMap::new();
         for path in paths {
             path.draw_rock(&mut grid);
@@ -34,7 +36,7 @@ fn main() {
         println!("Lowest rock: {}", lowest_rock);
         let start = Point { right: 500, down: 0 };
         let mut sand_count = 0;
-        while let Some(_) = fall_sand(&mut grid, start, lowest_rock) {
+        while let Some(_) = fall_sand(&mut grid, start, Some(lowest_rock), None) {
             sand_count += 1;
         }
         println!("Number of sand: {}", sand_count);
@@ -83,27 +85,36 @@ impl Path {
     }
 }
 
-fn fall_sand(grid: &mut HashMap<Point, Fill>, p: Point, lowest_rock: usize) -> Option<Point> {
-    if p.down >= lowest_rock {
-        // sand falls into the abyss
-        None
-    } else {
-        let down_centre = Point { right: p.right, down: p.down + 1 };
-        if !grid.contains_key(&down_centre) {
-            return fall_sand(grid, down_centre, lowest_rock);
-        }
-        if (p.right == 0) {
-            panic!("Reached right == 0");
-        }
-        let down_left = Point { right: p.right - 1, down: p.down + 1 };
-        if !grid.contains_key(&down_left) {
-            return fall_sand(grid, down_left, lowest_rock);
-        }
-        let down_right = Point { right: p.right + 1, down: p.down + 1 };
-        if !grid.contains_key(&down_right) {
-            return fall_sand(grid, down_right, lowest_rock);
-        }
-        grid.insert(p, Fill::Sand);
-        Some(p)
+fn fall_sand(grid: &mut HashMap<Point, Fill>, p: Point, lowest_rock: Option<usize>, floor_level: Option<usize>) -> Option<Point> {
+    match lowest_rock {
+        Some(low) => if p.down >= low {
+            // sand falls into the abyss
+            return None;
+        },
+        None => {}
+    };
+    match floor_level {
+        Some(floor) => if p.down == floor {
+            // on the floor
+            return Some(p);
+        },
+        None => {}
+    };
+    let down_centre = Point { right: p.right, down: p.down + 1 };
+    if !grid.contains_key(&down_centre) {
+        return fall_sand(grid, down_centre, lowest_rock, floor_level);
     }
+    if p.right == 0 {
+        panic!("Reached right == 0");
+    }
+    let down_left = Point { right: p.right - 1, down: p.down + 1 };
+    if !grid.contains_key(&down_left) {
+        return fall_sand(grid, down_left, lowest_rock, floor_level);
+    }
+    let down_right = Point { right: p.right + 1, down: p.down + 1 };
+    if !grid.contains_key(&down_right) {
+        return fall_sand(grid, down_right, lowest_rock, floor_level);
+    }
+    grid.insert(p, Fill::Sand);
+    return Some(p);
 }
