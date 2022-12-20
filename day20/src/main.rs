24b6db70a7c7686ff58b6e7ed6b9235e52944639
David Lang <davidlang42@gmail.com>
2022-12-20 16:53:52 +1100

    part2

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -7,34 +7,11 @@ fn main() {
         let filename = &args[1];
         let text = fs::read_to_string(&filename)
             .expect(&format!("Error reading from {}", filename));
-        let numbers: Vec<isize> = text.split("\r\n").map(|s| s.parse().unwrap()).collect();
-        let mut positions: Vec<usize> = Vec::new();
-        let mut zero_index = 0;
-        for i in 0..numbers.len() {
-            positions.push(i);
-            if numbers[i] == 0 {
-                zero_index = i;
-            }
-        }
-        //println!("{:?}", get_in_order(&positions, &numbers));
-        let loop_size = numbers.len() as isize - 1; // because being in the last place is the same as the first
-        for i in 0..numbers.len() {
-            let start = positions[i];
-            let mut finish = positions[i] as isize + numbers[i] % loop_size;
-            while finish < 0 {
-                finish += loop_size;
-            }
-            while finish >= numbers.len() as isize {
-                finish -= loop_size;
-            }
-            move_position(&mut positions, i, start, finish as usize);
-            //println!("{:?}", get_in_order(&positions, &numbers));
-        }
-        let zero_position = positions[zero_index];
-        let a = get_by_position(&positions, &numbers, zero_position + 1000);
-        let b = get_by_position(&positions, &numbers, zero_position + 2000);
-        let c = get_by_position(&positions, &numbers, zero_position + 3000);
-        println!("1000th: {}, 2000th: {}, 3000th: {}, SUM: {}", a, b, c, a+b+c);
+        let numbers: Vec<isize> = text.split("\r\n").map(|s| s.parse::<isize>().unwrap()).collect();
+        let (mut a, mut b, mut c) = run_mixing(&numbers, 1, 1);
+        println!("(1 round, factor 1) 1000th: {}, 2000th: {}, 3000th: {}, SUM: {}", a, b, c, a+b+c);
+        (a, b, c) = run_mixing(&numbers, 10, 811589153);
+        println!("(10 rounds, factor 811589153) 1000th: {}, 2000th: {}, 3000th: {}, SUM: {}", a, b, c, a+b+c);
     } else {
         println!("Please provide 1 argument: Filename");
     }
@@ -73,10 +50,33 @@ fn get_by_position(positions: &Vec<usize>, numbers: &Vec<isize>, position: usize
     panic!("Not found");
 }
 
-fn get_in_order(positions: &Vec<usize>, numbers: &Vec<isize>) -> Vec<isize> {
-    let mut vec = Vec::new();
+fn run_mixing(original_numbers: &Vec<isize>, rounds: usize, factor: isize) -> (isize, isize, isize) {
+    let numbers: Vec<isize> = original_numbers.iter().map(|n| n * factor).collect();
+    let mut positions: Vec<usize> = Vec::new();
+    let mut zero_index = 0;
     for i in 0..numbers.len() {
-        vec.push(get_by_position(positions, numbers, i));
+        positions.push(i);
+        if numbers[i] == 0 {
+            zero_index = i;
+        }
     }
-    vec
+    let loop_size = numbers.len() as isize - 1; // because being in the last place is the same as the first
+    for _ in 0..rounds {
+        for i in 0..numbers.len() {
+            let start = positions[i];
+            let mut finish = positions[i] as isize + numbers[i] % loop_size;
+            while finish < 0 {
+                finish += loop_size;
+            }
+            while finish >= numbers.len() as isize {
+                finish -= loop_size;
+            }
+            move_position(&mut positions, i, start, finish as usize);
+        }
+    }
+    let zero_position = positions[zero_index];
+    let a = get_by_position(&positions, &numbers, zero_position + 1000);
+    let b = get_by_position(&positions, &numbers, zero_position + 2000);
+    let c = get_by_position(&positions, &numbers, zero_position + 3000);
+    (a,b,c)
 }
